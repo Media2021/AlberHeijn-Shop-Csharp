c3f98f9ad5219140cacc175dc4b742012ea00f04
Media <95234709+Media2021@users.noreply.github.com>
2022-12-08 00:41:01 +0100

    Order

--- AlbertHeijnSynthesis/LogicLayer/OrderManager.cs
+++ AlbertHeijnSynthesis/LogicLayer/OrderManager.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DBlayer;
+using LogicLayer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,47 @@ namespace BusinessLayer
 {
     public  class OrderManager
     {
+        OrderDB orderDB = new OrderDB();
+        List<Order> orders = new List<Order>();
+
+
+        public OrderManager() 
+        { 
+
+        }
+
+        public List<Order> GetOrders()
+        { 
+            return orders;
+        }
+
+        public void UpdateOrderList()
+        {
+            orders.Clear();
+            orders.AddRange(orderDB.ReadOrders());
+
+        }
+
+        public void AddOrder(Order order)
+        {
+            orders.Add(order);
+            orderDB.CreateOrder(order);
+
+
+        }
+        public void DeleteOrder(Order order)
+        {
+            orders.Remove(order);
+            orderDB.DeleteOrder(order);
+
+
+        }
+        public void UpdateOrder(Order order)
+        {
+            orders.Clear();
+            orderDB.UpdateOrder(order);
+
+
+        }
     }
 }
